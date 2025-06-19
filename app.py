08384c08318a7445f7fdcb2220c21e9ebc3f31bf
Sreeharini.A <162721961+Sreeharini123@users.noreply.github.com>
2025-06-19 11:20:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,20 @@
 import streamlit as st
 from gtts import gTTS
-import os
 import tempfile
-from playsound import playsound
 
 st.set_page_config(page_title="AI Voice Generator", page_icon="🗣️")
 st.title("🗣️ AI Voice Generator for Mute Individuals")
 
 text = st.text_input("Enter your message:")
-language = "en"
 emotion = st.selectbox("Choose Emotion", ["Neutral", "Happy", "Sad"])
 
 if st.button("Speak"):
     if text:
-        tts = gTTS(text, lang=language)
-        temp_audio = tempfile.NamedTemporaryFile(delete=False, suffix=".mp3")
-        tts.save(temp_audio.name)
+        slow = True if emotion == "Sad" else False
+        tts = gTTS(text, lang='en', slow=slow)
 
-        audio_file = open(temp_audio.name, "rb")
-        audio_bytes = audio_file.read()
-        st.audio(audio_bytes, format='audio/mp3')
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as fp:
+            tts.save(fp.name)
+            st.audio(fp.name, format="audio/mp3")
     else:
         st.warning("Please enter some text.")
